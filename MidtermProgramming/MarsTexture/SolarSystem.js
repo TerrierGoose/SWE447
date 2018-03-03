0d0b69aacad348c2623c57fc1e0b9197917b8818
Joey Smith <joeymichaelsmith95@gmail.com>
2018-03-02 20:30:42 -0800

    Update SolarSystem.js

--- MidtermProgramming/MarsTexture/SolarSystem.js
+++ MidtermProgramming/MarsTexture/SolarSystem.js
@@ -1,39 +1,39 @@
 var SolarSystem = {
-  Sun : {
-    radius : 5,  
-    distance : 0,
-    year : 0,
-	//Push past HDR to force this color to not be dim lit
-	//Simulate Emission effect
-    color : [ 10.0, 10.0, 0.0, 1.0 ]
-  },
-  Mercury : {
-    radius : 0.5,
-    distance : 8,
-    year : 0.241,
-    color : [ 1.0, 0.0, 0.0, 1.0 ]
-  },
-  Venus : {
-    radius : 0.8,
-    distance : 12,
-    year : 0.615,
-    color : [ 1.0, 0.0, 1.0, 1.0 ]
-  },
-  Earth : {
-    radius : 1,
-    distance : 16,
-    year : 1,
-    color : [ 0.0, 0.0, 1.0, 1.0 ]
-  },
-  Moon : {
-    radius : 0.25,
-    distance : 2,
-    year : 0.0748,
-    color : [ 0.5, 0.5, 0.5, 1.0 ]
-  },
+//   Sun : {
+//     radius : 5,  
+//     distance : 0,
+//     year : 0,
+// 	//Push past HDR to force this color to not be dim lit
+// 	//Simulate Emission effect
+//     color : [ 10.0, 10.0, 0.0, 1.0 ]
+//   },
+//   Mercury : {
+//     radius : 0.5,
+//     distance : 8,
+//     year : 0.241,
+//     color : [ 1.0, 0.0, 0.0, 1.0 ]
+//   },
+//   Venus : {
+//     radius : 0.8,
+//     distance : 12,
+//     year : 0.615,
+//     color : [ 1.0, 0.0, 1.0, 1.0 ]
+//   },
+//   Earth : {
+//     radius : 1,
+//     distance : 16,
+//     year : 1,
+//     color : [ 0.0, 0.0, 1.0, 1.0 ]
+//   },
+//   Moon : {
+//     radius : 0.25,
+//     distance : 2,
+//     year : 0.0748,
+//     color : [ 0.5, 0.5, 0.5, 1.0 ]
+//   },
   Mars : {
     radius : 1,
-    distance : 20,
+    distance : 5,
     year : 1.88,
     color : [ 1.0, 0.0, 0.0, 1.0 ]
   }
